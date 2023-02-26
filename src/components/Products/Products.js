a2b4c0d744a3b2f13f07d60664f996c219662ea4
Karina Penkala <marsz@poczta.onet.pl>
2023-02-26 20:34:45 +0100

    Add PropTypes and map

---
+++ src/components/Products/Products.js
@@ -0,0 +1,33 @@
+import { useState } from 'react';
+import productsData from '../../data/products';
+import Product from '../Product/Product';
+
+const Products = () => {
+  const [products]  = useState(productsData);
+
+  return ( 
+    /*<section>
+        id={products[0].id}
+        name={products[0].name}
+        title={products[0].title} 
+        colors={products[0].colors}
+        sizes={products[0].sizes}
+        basePrice={products[0].basePrice}
+    </section>*/
+    <section>
+        {products.map (product => <Product key={product.id} {...products[0]} {...products[1]} />)}
+     </section>
+ 
+     /* <Product
+        id={products[1].id}
+        name={products[1].name}
+        title={products[1].title}
+        colors={products[1].colors}
+        sizes={products[1].sizes}
+        basePrice={products[1].basePrice} />
+    </section>*/
+    
+  );
+};
+
+export default Products;
